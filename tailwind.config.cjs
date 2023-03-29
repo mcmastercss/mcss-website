8bd4ce311fa5ca0fe3c2d14493c6d99ea3e976b1
Viransh Shah <shahv47@mcmaster.ca>
2023-03-29 00:06:19 -0400

    mobile responsive marquee, requires a fix for the space between the ending image and starting image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 20s linear infinite",
+        marquee2: "marquee2 20s linear infinite",
+        marquee3: "marquee3 15s linear infinite",
+        marquee4: "marquee4 15s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-160%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(160%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        marquee3: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-258%)" },
+        },
+        marquee4: {
+          "0%": { transform: "translateX(258%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   plugins: [
     require("tailwind-scrollbar")({ nocompatible: true }),
