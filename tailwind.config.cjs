532486086a02cea1c512146e4a01d3614547e513
Krish120003 <krish120003@gmail.com>
2023-09-08 22:53:04 -0400

    fix: adjust marquee animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,27 +4,12 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        marquee: "marquee 10s linear infinite",
-        marquee2: "marquee2 10s linear infinite",
-        marquee3: "marquee3 10s linear infinite",
-        marquee4: "marquee4 10s linear infinite",
+        marquee: "marquee 15s linear infinite",
       },
       keyframes: {
         marquee: {
           "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        marquee2: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-        marquee3: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-240%)" },
-        },
-        marquee4: {
-          "0%": { transform: "translateX(240%)" },
-          "100%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(calc(-100% - 1rem))" },
         },
       },
     },
